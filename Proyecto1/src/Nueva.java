670b33d0dfa474b3c4288be17c677b12baca3656
Cesar <cesargarciaviana@gmail.com>
2022-02-18 13:46:33 +0100

    arreglando conflicto

--- Proyecto1/src/Nueva.java
+++ Proyecto1/src/Nueva.java
@@ -2,13 +2,10 @@
 public class Nueva {
 	public static void main(String[] args) {
 		System.out.println("Hola");
-<<<<<<< HEAD
 		System.out.println("Conflicto");
 		System.out.println("Conflicto2");
-=======
 		System.out.println("Que tal");
 		System.out.println("Conflicto1");
 		System.out.println("Conflicto1 bis");
->>>>>>> branch 'master' of git@github.com:chechare6/RepasoED.git
 	}
 }
